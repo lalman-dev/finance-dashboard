41b2317bb4f4466a7e641b68e8bfca2c8fd59cb8
Lalman <lalmanchaudhary2@gmail.com>
2026-04-03 18:58:26 +0530

    feat(transactions): integrate add transaction modal - connect modal visibility state with transactions section - trigger modal from admin action button - enable transaction creation flow from UI

--- src/features/transactions/TransactionsSection.tsx
+++ src/features/transactions/TransactionsSection.tsx
@@ -4,23 +4,30 @@ import Card from "@/src/components/ui/Card";
 import Filters from "./Filters";
 import TransactionsTable from "./TransactionsTable";
 import { useFinanceStore } from "@/src/store/useFinanceStore";
+import { useState } from "react";
+import AddTransactionModal from "./AddTransactionModal";
 
 export default function TransactionsSection() {
+  const [open, setOpen] = useState(false);
   const { role } = useFinanceStore();
 
   return (
     <Card>
-      <div className="flex items-center justify-between mb-4">
+      <div className="flex items-center justify-between mb-4 ">
         <h3 className="text-lg font-medium">Transactions</h3>
 
         {role === "admin" && (
-          <button className="px-4 py-2 bg-indigo-500 text-white rounded-lg hover:bg-indigo-600 transition">
+          <button
+            onClick={() => setOpen(true)}
+            className="px-4 py-2 bg-indigo-500 text-white rounded-lg hover:bg-indigo-600 transition"
+          >
             + Add Transaction
           </button>
         )}
       </div>
       <Filters />
       <TransactionsTable />
+      <AddTransactionModal open={open} onClose={() => setOpen(false)} />
     </Card>
   );
 }
