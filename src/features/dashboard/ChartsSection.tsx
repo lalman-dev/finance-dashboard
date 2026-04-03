5ff858d3e09c32c6e1186cf79e2688380155f2b1
Lalman <lalmanchaudhary2@gmail.com>
2026-04-03 21:11:14 +0530

    feat(dashboard): enhance dashboard layout and visual structure - improve charts section layout and responsiveness - refine summary cards with better spacing and hierarchy - align dashboard UI with fintech-style design

--- src/features/dashboard/ChartsSection.tsx
+++ src/features/dashboard/ChartsSection.tsx
@@ -4,14 +4,18 @@ import SpendingPieChart from "@/src/components/charts/SpendingPieChart";
 
 export default function ChartsSection() {
   return (
-    <div className="grid md:grid-cols-2 gap-4">
+    <div className="grid md:grid-cols-2 gap-4 ">
       <Card>
-        <h3 className="text-sm text-gray-500 mb-2">Balance Trend</h3>
+        <h3 className="text-sm text-gray-500 mb-2 dark:text-gray-400">
+          Balance Trend
+        </h3>
         <BalanceLineChart />
       </Card>
 
       <Card>
-        <h3 className="text-sm text-gray-500 mb-2">Spending Breakdown</h3>
+        <h3 className="text-sm text-gray-500 mb-2 dark:text-gray-400">
+          Spending Breakdown
+        </h3>
         <SpendingPieChart />
       </Card>
     </div>
--- src/features/dashboard/SummaryCards.tsx
+++ src/features/dashboard/SummaryCards.tsx
@@ -19,18 +19,18 @@ export default function SummaryCards() {
   return (
     <div className="grid md:grid-cols-3 gap-4">
       <Card>
-        <p className="text-sm text-gray-500">Balance</p>
-        <h2 className="text-xl font-semibold">₹{balance}</h2>
+        <p className="text-sm text-gray-500 dark:text-gray-400">Balance</p>
+        <h2 className="text-xl font-semibold dark:text-white">₹{balance}</h2>
       </Card>
 
       <Card>
-        <p className="text-sm text-gray-500">Income</p>
-        <h2 className="text-xl font-semibold">₹{income}</h2>
+        <p className="text-sm text-gray-500 dark:text-gray-400">Income</p>
+        <h2 className="text-xl font-semibold dark:text-white">₹{income}</h2>
       </Card>
 
       <Card>
-        <p className="text-sm text-gray-500">Expenses</p>
-        <h2 className="text-xl font-semibold">₹{expense}</h2>
+        <p className="text-sm text-gray-500 dark:text-gray-400">Expenses</p>
+        <h2 className="text-xl font-semibold dark:text-white">₹{expense}</h2>
       </Card>
     </div>
   );
