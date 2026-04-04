be7ed2877d13839038a967c3a03afd9df979ddc8
Lalman <lalmanchaudhary2@gmail.com>
2026-04-04 15:05:28 +0530

    feat(ui): enhance user experience with framer motion animations

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,20 +1,30 @@
+"use client";
+
 import RoleToggle from "@/src/components/ui/RoleToggle";
 import ThemeToggle from "@/src/components/ui/ThemeToggle";
 import ChartsSection from "@/src/features/dashboard/ChartsSection";
 import SummaryCards from "@/src/features/dashboard/SummaryCards";
 import TransactionsSection from "@/src/features/transactions/TransactionsSection";
+import { motion } from "framer-motion";
 
 export default function DashboardPage() {
   return (
-    <main className="p-6 space-y-8">
+    <motion.main
+      className="p-6 space-y-8"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.4 }}
+    >
       <div className="flex justify-between items-center">
         <h1 className="text-2xl font-semibold">Dashboard</h1>
-        <ThemeToggle/>
-        <RoleToggle />
+        <div className="flex gap-3">
+          <RoleToggle />
+          <ThemeToggle />
+        </div>
       </div>
       <SummaryCards />
       <ChartsSection />
       <TransactionsSection />
-    </main>
+    </motion.main>
   );
 }
